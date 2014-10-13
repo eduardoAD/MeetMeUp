351301350fd718c20d18b4dd7c3639ca45e4f786
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-13 13:53:53 -0500

    task #1 complete

--- MeetMeUp/ViewController.m
+++ MeetMeUp/ViewController.m
@@ -9,14 +9,27 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property NSArray *events;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSURL *url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=35476c66197967107464723a5869147"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+
+        NSString *jsonString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"%@",jsonString);
+
+        NSError *jsonError = nil;
+        self.events = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+
+
+        NSLog(@"Connection error: %@", connectionError);
+        NSLog(@"JSON error: %@", jsonError);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
